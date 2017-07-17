8acb41e23b07f97bce70fe6b262ad70676647949
sky0014 <sky-wang@qq.com>
2017-07-17 19:48:37 +0800

    fix gen bug

--- src/tool.js
+++ src/tool.js
@@ -4,7 +4,7 @@
  */
 export function gen(n) {
   if (n > 7) {
-    return random(7) + random(n - 7);
+    return gen(7) + gen(n - 7);
   } else {
     return Math.floor(Math.random() * (1 << (n * 4)))
       .toString(16)
