b2bedaa236d7c2c6eea0e5e618a9c44dc8fc3fac
sky0014 <sky-wang@qq.com>
2017-08-04 17:17:53 +0800

    optimize bindAll

--- src/bindAll.js
+++ src/bindAll.js
@@ -1,7 +1,31 @@
-//bind all function
-Object.getOwnPropertyNames(Object.getPrototypeOf(this)).forEach(name => {
-  let value = this[name];
-  if (typeof value === "function") {
-    this[name] = value.bind(this);
+/**
+ * 遍历对象，绑定全部函数
+ * @param {*} target 要绑定到的目标
+ * @param {*} proto 要执行绑定的对象，省略则以target为目标
+ */
+function bindAll(target, proto) {
+  if (proto === undefined) {
+    proto = Object.getPrototypeOf(target);
   }
-});
+  Object.getOwnPropertyNames(proto).forEach(name => {
+    let value = target[name];
+    if (typeof value === "function") {
+      target[name] = value.bind(target);
+    }
+  });
+}
+
+class A {
+  constructor() {
+    //bind A
+    bindAll(this, A.prototype);
+    //bind B or other sub class
+    bindAll(this);
+  }
+}
+
+class B extends A {
+  constructor() {
+    super();
+  }
+}
