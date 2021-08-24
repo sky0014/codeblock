7c84d4c2c5b5760a2279bf7a38dacbd93c1af65b
sky0014 <sky-wang@qq.com>
2021-08-24 14:31:24 +0800

    Update bindAll.js

--- src/bindAll.js
+++ src/bindAll.js
@@ -2,25 +2,38 @@
  * 遍历对象，绑定全部函数
  * @param {*} target 要绑定到的目标
  * @param {*} proto 要执行绑定的对象，省略则以target为目标
+ * @returns {void}
  */
-function bindAll(target, proto) {
-  if (proto === undefined) {
-    proto = Object.getPrototypeOf(target);
+export function bindAll(target) {
+  const that = target;
+  const bindFunctions = {};
+  while (target && target !== Object.prototype) {
+    // eslint-disable-next-line
+    Object.getOwnPropertyNames(target).forEach(name => {
+      const config = Object.getOwnPropertyDescriptor(target, name);
+      if (config) {
+        if (config.get || config.set) {
+          return;
+        }
+
+        if (name !== "constructor" && typeof that[name] === "function") {
+          // 去重存储函数名
+          bindFunctions[name] = true;
+        }
+      }
+    });
+    target = Object.getPrototypeOf(target);
   }
-  Object.getOwnPropertyNames(proto).forEach(name => {
-    let value = target[name];
-    if (typeof value === "function") {
-      target[name] = value.bind(target);
-    }
+  Object.keys(bindFunctions).forEach(name => {
+    that[name] = that[name].bind(that);
   });
 }
 
+
 class A {
   constructor() {
-    //bind A
-    bindAll(this, A.prototype);
-    //bind B or other sub class
-    bindAll(this);
+    //bind A and B and etc...
+    bindAll(this);    
   }
 }
 
